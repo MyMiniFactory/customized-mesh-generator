eb054413517dc75e0c045d9a57d5c6d93c595c97
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-07-08 17:16:43 +0100

    perf(union): use pymesh for union

--- mesh_union/utils.py
+++ mesh_union/utils.py
@@ -1,5 +1,6 @@
 import time
 import trimesh
+import pymesh
 from mesh_union.logger import logger
 
 def dict_to_tuple(_dict):
@@ -8,17 +9,9 @@ def dict_to_tuple(_dict):
 def union(meshes):
     start_time = time.time()
 
-    if len(meshes) < 2:
-        logger.debug("Too few meshes. Union skipped")
-        return meshes[0]
-
-    union_mesh, remaining_meshes = meshes[0], meshes[1:]
-    for mesh in remaining_meshes:
-        try:
-            union_mesh = union_mesh.union(mesh)
-        except Exception as e:
-            logger.error(e)
-            raise e
+    union_mesh = pymesh.CSGTree({
+        "union": [{"mesh": trimesh_to_pymesh(mesh)} for mesh in meshes]
+    }).mesh
 
     end_time = time.time()
 
@@ -26,7 +19,16 @@ def union(meshes):
 
     logger.debug("Union of {} files. Time taken: {:6.2f}".format(len(meshes), total_time))
 
-    return union_mesh
+    return pymesh_to_trimesh(union_mesh)
+
+def trimesh_to_pymesh(trimesh_mesh):
+    return pymesh.form_mesh(trimesh_mesh.vertices, trimesh_mesh.faces)
+
+def pymesh_to_trimesh(pymesh_mesh):
+    return trimesh.Trimesh(
+        vertices = pymesh_mesh.vertices,
+        faces = pymesh_mesh.faces
+    )
 
 
 def load_mesh(path, file_type = 'stl'):
