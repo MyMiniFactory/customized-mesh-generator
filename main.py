5f976a31a1768ceca3801068245cd745ebda3e05
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-07-08 17:34:44 +0100

    refactor: process input json in separate function

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ from mesh_union.logger import logger
 from mesh_union.tree import Node
 from mesh_union.utils import union
 
+def get_metadata(json_file_or_encoded):
+    try:
+        if json_file_or_encoded.endswith('.json'): # uses json file
+            with open(json_file_or_encoded) as f:
+                return json.load(f)
+        else:
+            return json.loads(json_file_or_encoded)
+    except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
+        logger.error("Error loading json - {}".format(e))
+        sys.exit(1)
 
 if __name__ == '__main__':
     args = sys.argv[1:]
@@ -22,18 +32,7 @@ if __name__ == '__main__':
     # second arg is the path to the output file
     [json_file_or_encoded, output_path] = args
 
-    metadata = None
-
-    try:
-        if json_file_or_encoded.endswith('.json'): # uses json file
-            with open(json_file_or_encoded) as f:
-                metadata = json.load(f)
-        else:
-            metadata = json.loads(json_file_or_encoded)
-    except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
-        logger.error("Error loading json - {}".format(e))
-        sys.exit(1)
-    
+    metadata = get_metadata(json_file_or_encoded)
 
     tree = Node.from_json(metadata)
 
