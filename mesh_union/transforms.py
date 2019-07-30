8efc51c7b9eea9724226647b9b72f25b7758558f
Adrian Delgado <adrian.delgadovlaic@gmail.com>
2019-07-30 18:27:21 +0100

    fix(transforms): use correct euler order
    Apply rotations in order 'XYZ' (instead of 'ZYX').

--- mesh_union/transforms.py
+++ mesh_union/transforms.py
@@ -17,21 +17,29 @@ def _euler2mat(x = 0, y = 0, z = 0):
     sin_ψ, sin_θ, sin_φ = math.sin(x), math.sin(y), math.sin(z)
     cos_ψ, cos_θ, cos_φ = math.cos(x), math.cos(y), math.cos(z)
 
-    m11 = cos_θ * cos_φ
-    m12 = sin_ψ * sin_θ * cos_φ - cos_ψ * sin_φ
-    m13 = cos_ψ * sin_θ * cos_φ + sin_ψ * sin_φ
-    m21 = cos_θ * sin_φ
-    m22 = sin_ψ * sin_θ * sin_φ + cos_ψ * cos_φ
-    m23 = cos_ψ * sin_θ * sin_φ - sin_ψ * cos_φ
-    m31 = -sin_θ
-    m32 = sin_ψ * cos_θ
-    m33 = cos_ψ * cos_θ
-
-    return np.array([
-        [ m11, m12, m13 ],
-        [ m21, m22, m23 ],
-        [ m31, m32, m33 ],
+    rotation_matrix_x = np.array([
+        [   1.  ,   0.  ,   0.  ],
+        [   0.  ,  cos_ψ, -sin_ψ],
+        [   0.  ,  sin_ψ,  cos_ψ],
     ])
+
+    rotation_matrix_y = np.array([
+        [  cos_θ,   0.  ,  sin_θ],
+        [   0.  ,   1.  ,   0.  ],
+        [ -sin_θ,   0.  ,  cos_θ],
+    ])
+
+    rotation_matrix_z = np.array([
+        [  cos_φ, -sin_φ,   0.  ],
+        [  sin_φ,  cos_φ,   0.  ],
+        [   0.  ,   0.  ,   1.  ],
+    ])
+
+    '''
+        (https://en.wikipedia.org/wiki/Euler_angles#Rotation_matrix)
+        If order is 'XYZ', then rotation_matrix = R(x) * R(y) * R(z)
+    '''
+    return rotation_matrix_x @ rotation_matrix_y @ rotation_matrix_z
 
 def _scale2mat(x = 0, y = 0, z = 0):
     return np.diag([x, y, z])
